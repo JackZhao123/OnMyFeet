63b30301bb5326124adaab771f603477c4a96daf
JackZhao123 <zxiongbin@gmail.com>
2016-02-16 19:40:03 -0500

    catch_Access_Token

--- onMyFeet/AppDelegate.swift
+++ onMyFeet/AppDelegate.swift
@@ -17,9 +17,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        if let launchOptions = launchOptions {
+            let s = launchOptions["UIApplicationLaunchOptionsURLKey"] as! NSURL
+            access_token = String(s)
+        } else {
+            access_token = "No Access_token available"
+        }
+        
         return true
     }
-
+    
+    func application(app: UIApplication, openURL url: NSURL, options: [String : AnyObject]) -> Bool {
+        access_token = String(url)
+        return true
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -36,6 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
     }
 
     func applicationWillTerminate(application: UIApplication) {
--- onMyFeet/ViewController.swift
+++ onMyFeet/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import SafariServices
+
+var access_token:String?
 
 class ViewController: UIViewController {
-
+//    MARK: Outlet
+    @IBOutlet weak var logInBtn: UIButton!
+    @IBOutlet weak var tokenTextView: UITextView!
+    
+//    MARK: Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let access_token = access_token {
+            self.tokenTextView.text = access_token
+        } else {
+            self.tokenTextView.text = "No Token Aviable"
+        }
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "didBecomeActive", name: UIApplicationDidBecomeActiveNotification, object: nil)
+        
+        initSubView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func didBecomeActive() {
+        self.tokenTextView.text = access_token
+    }
 
+    func initSubView() {
+        self.logInBtn.layer.cornerRadius = 5.0
+        self.logInBtn.clipsToBounds = true
+    }
 
+    @IBAction func logIn(sender: AnyObject) {
+        //print("Logging in");
+        UIApplication.sharedApplication().openURL(NSURL(string: "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=227GMP&redirect_uri=onmyfeet://&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800")!)
+    }
 }
 
